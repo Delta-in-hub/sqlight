96e30d6a33adeca752fdde5b6cf9704ce8d025fd
Delta <DeltaMail@qq.com>
2022-01-06 17:54:36 +0800

    configure cmake

--- test/main.cc
+++ test/main.cc
@@ -2,11 +2,12 @@
 #include "fmt/format.h"
 #include "fmt/color.h"
 #include "bitwise.h"
+#include <thread>
 
 #define Print(arg, ...) fmt::print(fmt::fg(fmt::color::aqua), arg, __VA_ARGS__)
 using namespace testing;
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions)
+TEST(Example,test)
 {
     // Expect two strings not to be equal.
     EXPECT_STRNE("hello", "world");
@@ -21,6 +22,7 @@ TEST(HelloTest, BasicAssertions)
 
 int main(int argc, char **argv)
 {
+    fmt::print("{}\n",std::thread::hardware_concurrency());
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
